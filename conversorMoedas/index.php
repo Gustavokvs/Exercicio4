7d47746844f5b0d29c3a8df8ecb6b45e89f8418e
Gustavokvs <gustavokruger514@gmail.com>
2025-06-10 00:20:47 -0300

    Feito parcialmente conversor de moeda, de maneira nova

---
+++ conversorMoedas/index.php
@@ -0,0 +1,44 @@
+<?php require_once "conversor.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de moedas</title>
+</head>
+
+<body>
+
+    <form method="post">
+        <label>R$ <input type="number" name="moedaReais" required></label><br><br>
+        <label>Dolar/Euro <input type="text" name="escolha" required></label><br><br>
+        <label>Cotação do Dólar: <input type="number" step="0.01" name="cotacaoDolar" required></label><br><br>
+        <label>Cotação do Euro: <input type="number" step="0.01" name="cotacaoEuro" required></label><br><br>
+
+        <button type="submit">Calcular</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $conv = new Conversor();
+
+
+        $escolha = $_POST['escolha'];
+        $moeda = $_POST['moedaReais'];
+        $conv->setEscolha($escolha);
+        $conv->setMoedaDolar($_POST['cotacaoDolar']);
+        $conv->setMoedaEuro($_POST['cotacaoEuro']);
+        $conv->setMoeda($moeda);
+
+
+        echo "<h3>Resultado:</h3>";
+        echo $conv->converter($conv->getEscolha());
+    }
+    ?>
+
+</body>
+
+</html>
